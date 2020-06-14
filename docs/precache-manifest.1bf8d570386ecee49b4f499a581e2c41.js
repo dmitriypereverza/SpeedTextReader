48dc9893544b8bef894cadf23355c290e6f9d763
Дмитрий Переверза <d.pereverza@worksolutions.ru>
2020-06-14 23:04:12 +0300

    add docs

---
+++ docs/precache-manifest.1bf8d570386ecee49b4f499a581e2c41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac487e86c75e04b408f0acae637e971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9c7e3430489cc33abf6",
+    "url": "/static/css/2.56974eb5.chunk.css"
+  },
+  {
+    "revision": "5fe89d04b8f885789951",
+    "url": "/static/css/main.28a40b2c.chunk.css"
+  },
+  {
+    "revision": "a9c7e3430489cc33abf6",
+    "url": "/static/js/2.e31a1e8a.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/static/js/2.e31a1e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fe89d04b8f885789951",
+    "url": "/static/js/main.1429d538.chunk.js"
+  },
+  {
+    "revision": "3df2fde73b422664b46d",
+    "url": "/static/js/runtime-main.aa1d3b78.js"
+  }
+]);
